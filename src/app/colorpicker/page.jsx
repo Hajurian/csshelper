6f8235b925a7a5fa1ed34de54048f475106705f2
Hajurian <julianmoet1@gmail.com>
2024-07-01 19:04:20 -0400

    Made color picker responsive

--- src/app/colorpicker/page.jsx
+++ src/app/colorpicker/page.jsx
@@ -3,20 +3,20 @@ import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/css";
 import { MdContentCopy } from "react-icons/md";
 export default function ColorPage() {
-  const [color, setColor] = useColor("#000");
+  const [color, setColor] = useColor("#1fb6ff");
   return (
     <main className="w-screen h-screen flex justify-center">
-      <section className="w-4/5 h-full flex justify-center">
-        <div className="w-1/3 h-max p-8 flex flex-col border-2 border-gray-light rounded-md">
-          <h1 className="text-center mb-4 text-5xl text-blue font-extrabold">
+      <section className="w-4/5 h-full flex justify-center animate-fade-down">
+        <div className="w-[20rem] md:w-[25rem] lg:w-[25rem] xxl:w-1/3 h-max p-8 flex flex-col border-2 border-gray-light rounded-md">
+          <h1 className="text-center mb-4 text-2xl xxl:text-5xl text-blue font-extrabold">
             Color Picker
           </h1>
           <div
-            className="w-32 h-32 mx-auto mb-16 border-2 "
+            className="w-32 h-32 mx-auto mb-16 border-2 border-gray-light rounded-lg"
             style={{ backgroundColor: color.hex }}
           ></div>
           <ColorPicker hideInput={["hsv"]} color={color} onChange={setColor} />
-          <div className="w-full flex justify-center">
+          <div className="w-full lg:flex xxl:flex justify-center">
             <button
               onClick={() => {
                 navigator.clipboard.writeText(
@@ -25,7 +25,7 @@ export default function ColorPage() {
                   )},${Math.round(color.rgb.b)},${color.rgb.a})`
                 );
               }}
-              className="font-bold flex items-center justify-center py-2 border-2 w-1/3 mt-4 mx-auto rounded-xl bg-blue border-none text-background text-2xl cursor-pointer  hover:scale-105 transition-all shadow-md shadow-code"
+              className="buttons"
             >
               <MdContentCopy color={"#d3dce6"} /> Copy RGB
             </button>
@@ -33,7 +33,7 @@ export default function ColorPage() {
               onClick={() => {
                 navigator.clipboard.writeText(`${color.hex}`);
               }}
-              className="font-bold flex items-center justify-center py-2 border-2 w-1/3 mt-4 mx-auto rounded-xl bg-blue border-none text-background text-2xl cursor-pointer  hover:scale-105 transition-all shadow-md shadow-code"
+              className="buttons"
             >
               <MdContentCopy color={"#d3dce6"} /> Copy HEX
             </button>
